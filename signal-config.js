d98ae6f74f83083d988f76d7d630ef081a613d20
hobbit12b <hobbit12b@gmail.com>
2026-08-16 23:53:41 +0200

    Add signaling endpoint configuration placeholder

---
+++ signal-config.js
@@ -0,0 +1,3 @@
+// Vul hier na deployment alleen het HTTPS-adres van de signalingservice in.
+// Voorbeeld: window.POPPENKAST_SIGNAL_URL = 'https://poppenkast-signaling.<jouw-subdomein>.workers.dev';
+window.POPPENKAST_SIGNAL_URL = '';
