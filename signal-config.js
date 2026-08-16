5b2702a76b0be9035ba7bbd3a32ffc34ab52f790
hobbit12b <hobbit12b@gmail.com>
2026-08-17 00:15:26 +0200

    Connect QR flow to Cloudflare signaling worker

--- signal-config.js
+++ signal-config.js
@@ -1,3 +1,2 @@
-// Vul hier na deployment alleen het HTTPS-adres van de signalingservice in.
-// Voorbeeld: window.POPPENKAST_SIGNAL_URL = 'https://poppenkast-signaling.<jouw-subdomein>.workers.dev';
-window.POPPENKAST_SIGNAL_URL = '';
+// HTTPS-adres van de tijdelijke signalingservice voor Poppenkastspiegel.
+window.POPPENKAST_SIGNAL_URL = 'https://poppenkastspiegel.hobbit12b.workers.dev';
